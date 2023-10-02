ab04d1ee05c8bfda25b71d4063cf0563ae18bdb7
Mohd Yunus <112767616+Yunuslala@users.noreply.github.com>
2023-10-02 16:16:49 +0530

    added swagger api docs

--- models/PaymentDetails.Model.js
+++ models/PaymentDetails.Model.js
@@ -4,7 +4,7 @@ const PaymentDetailsSchema = mongoose.Schema({
     cardName: String,
     cardNumber: String,
     cvv: String,
-    expDate: String,
+    expDate: String
 });
 
 const PaymentDetailsModel = mongoose.model("PaymentDetails", PaymentDetailsSchema);
